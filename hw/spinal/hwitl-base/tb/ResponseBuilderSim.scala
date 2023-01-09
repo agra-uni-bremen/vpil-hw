4f4bdb551ff7778111d8619575b5ccc5bdce6005
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2023-01-09 16:19:30 +0100

    init response builder testbench + minor

--- hw/spinal/hwitl-base/tb/ResponseBuilderSim.scala
+++ hw/spinal/hwitl-base/tb/ResponseBuilderSim.scala
@@ -5,14 +5,18 @@ import spinal.core.sim._
 
 object ResponseBuilderSim extends App {
   Config.sim.compile(ResponseBuilder()).doSim { dut =>
-    List().foreach(_ #= false)
-    List().foreach(_ #= 0)
+    List(dut.io.ctrl.enable, dut.io.ctrl.clear, dut.io.data.irq, dut.io.txFifo.ready).foreach(_ #= false)
+    List(dut.io.data.readData, dut.io.data.ack).foreach(_ #= 0)
+    dut.io.ctrl.respType #= ResponseType.noPayload
+
     // Fork a process to generate the reset and the clock on the dut
     dut.clockDomain.forkStimulus(period = 10)
     dut.clockDomain.waitRisingEdge()
-    dut.io.enable #= true
+    dut.io.ctrl.enable #= true
     dut.clockDomain.waitRisingEdge()
-    dut.io.enable #= false
+    dut.io.ctrl.enable #= false
+    dut.clockDomain.waitRisingEdge()
+    dut.clockDomain.waitRisingEdge()
     dut.clockDomain.waitRisingEdge()
     dut.clockDomain.waitRisingEdge()
     dut.clockDomain.waitRisingEdge()
