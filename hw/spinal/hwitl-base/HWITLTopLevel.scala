9d1c49baa20cda80432ec7c80ea13921ba9e7305
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2023-01-18 16:21:40 +0100

    init top level wiring, wrap some modules, commit cannot build yet

--- hw/spinal/hwitl-base/HWITLTopLevel.scala
+++ hw/spinal/hwitl-base/HWITLTopLevel.scala
@@ -1,27 +1,41 @@
 package hwitlbase
 
 import spinal.core._
+import spinal.lib._
+import spinal.lib.com.uart._
 
 /**
- * Configuration class for HWITL config
- * @param 
- * 
+ * Default configuration parameters for the HWitL system
  */
 case class HWITLConfig(
-    uartBaudRate : Long = 9600,
+    uartBaudRate : Long = 115200,
     uartTxFifoSize : Int = 16,
-    uartRxFifoSize : Int = 16
-){
-
-}
+    uartRxFifoSize : Int = 16,
+    timeoutDelay : TimeNumber = 2 ms
+){}
 
 // Hardware definition
-case class HWITLTopLevel() extends Component {
+case class HWITLTopLevel(config : HWITLConfig) extends Component {
   val io = new Bundle {
+    val uart = master(new Uart())
   }
 
+  //******** HW in the Loop elements *********
+  val uartCtrl = new UartCtrl()
+  val rxFifo = new StreamFifo(dataType = Bits(8 bits), depth = config.uartRxFifoSize)
+  val txFifo = new StreamFifo(dataType = Bits(8 bits), depth = config.uartTxFifoSize)
+  val serParConv = new SerialParallelConverter(8,32)
+  val timeout = Timeout(config.timeoutDelay)
+  val builder = new ResponseBuilder()
+  val tic = new TranslatorInterface() // acts as simplebus master
+
+
+  //******** Peripherals *********
+  val no_map = new NoMapPeriphral()
+
+  //******** Memory mappings *********
 }
 
 object HWITLTopLevelVerilog extends App {
-  Config.spinal.generateVerilog(HWITLTopLevel())
+  Config.spinal.generateVerilog(new HWITLTopLevel(HWITLConfig()))
 }
--- hw/spinal/hwitl-base/tb/HWITLTopLevelSim.scala
+++ hw/spinal/hwitl-base/tb/HWITLTopLevelSim.scala
@@ -4,9 +4,9 @@ import spinal.core._
 import spinal.core.sim._
 
 object HWITLTopLevelSim extends App {
-  Config.sim.compile(HWITLTopLevel()).doSim { dut =>
+  Config.sim.compile(HWITLTopLevel(HWITLConfig())).doSim { dut =>
     // Fork a process to generate the reset and the clock on the dut
-    dut.clockDomain.forkStimulus(period = 10)
+    dut.clockDomain.forkStimulus(period = 82)
 
     for (idx <- 0 to 99) {
       // Wait a rising edge on the clock
