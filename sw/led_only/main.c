38a1c6300ffb83fe5a2d55cd1a71a33360cd6c53
Pascal Pieper <accounts@pascalpieper.de>
2023-01-25 10:11:59 +0100

    Initial LED test sw

--- sw/led_only/main.c
+++ sw/led_only/main.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "../common/irq.h"
 
 typedef uint32_t BUS_BRIDGE_TYPE;
 static volatile BUS_BRIDGE_TYPE * const BUS_BRIDGE_START = (BUS_BRIDGE_TYPE * const) 0x50000000;
 static volatile BUS_BRIDGE_TYPE * const BUS_BRIDGE_END   = (BUS_BRIDGE_TYPE * const) 0x5000000F; // INCLUSIVE
 static const unsigned BUS_BRIDGE_ITR = 2;
-static volatile char * const TERMINAL_ADDR = (char * const)0x20000000;
+
 
 static const unsigned num_words = (BUS_BRIDGE_END - BUS_BRIDGE_START) + 1;		// INCLUSIVE
 
@@ -13,43 +14,33 @@ void read_stuff() {
 	for (int i = 0; i < num_words; i++) {
 		const BUS_BRIDGE_TYPE datum = BUS_BRIDGE_START[i];
 		for(int c = 0; c < sizeof(BUS_BRIDGE_TYPE); c++) {
-			*TERMINAL_ADDR = ((uint8_t*)&datum)[c];
+			printf("%0X ", ((uint8_t*)&datum)[c]);
 		}
-		*TERMINAL_ADDR = '\n';
+		printf("\n");
 	}
 }
 
-void write_stuff() {
-	for (int i = 0; i < num_words; i++) {
-		BUS_BRIDGE_TYPE datum;
-		for(int c = 0; c < sizeof(BUS_BRIDGE_TYPE); c++) {
-			((uint8_t*)&datum)[c] = 'a' + (i+c);
-			*TERMINAL_ADDR = ((uint8_t*)&datum)[c];
-		}
-		BUS_BRIDGE_START[i] = datum;
-		*TERMINAL_ADDR = '\n';
-	}
-}
 
-volatile int was_itr_triggered = 0;
-void virtual_bus_irq_handler() {
-	static const char* hi = "Interrupt was triggered\n";
-	was_itr_triggered = 1;
-	for(int i = 0; hi[i]; i++)
-		*TERMINAL_ADDR = hi[i];
+
+void write_stuff(unsigned delay) {
+	printf("Write LED with delay of %d clock cycles\n", delay);
+	for (unsigned i = 0; i < 256; i++) {
+		printf("Writing %0X...\n", i);
+		*BUS_BRIDGE_START = i;
+		uint64_t now = *mtime;
+		while(now + delay >= *mtime) {
+			asm volatile ("nop");
+		}
+	}
 }
 
 int main() {
-	register_interrupt_handler(BUS_BRIDGE_ITR, virtual_bus_irq_handler);
 
-	read_stuff();
-
-	/*
-	while(!was_itr_triggered)
-		asm volatile ("wfi");
-	*/
-
-	write_stuff();
+	write_stuff(100);
+	
+	write_stuff(0);
+	
+	write_stuff(10000);
 
 	return 0;
 }
