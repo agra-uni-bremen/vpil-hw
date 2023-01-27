2f529fb906c97c2ae9fa190639a17e43173f4e66
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2023-01-27 15:30:42 +0100

    start fixing software for both gpio banks, still bugged, crashes after a while

--- sw/dual_bank_gpio/main.c
+++ sw/dual_bank_gpio/main.c
@@ -10,13 +10,13 @@ static const unsigned BUS_BRIDGE_ITR = 2;
 
 
 struct MRV32_GPIO {
-	volatile uint8_t direction; // direction of pins, 0: input (read pin), 1: output (write pin)
-	volatile uint8_t input; // incomming, read pin values
-	volatile uint8_t output; // outgoing pin values
+	volatile uint32_t direction; // direction of pins, 0: input (read pin), 1: output (write pin)
+	volatile uint32_t output; // outgoing pin values
+	volatile uint32_t input; // incomming, read pin values
 };
 
 struct MRV32_INTLED {
-	volatile uint8_t val;
+	volatile uint32_t val;
 };
 
 static struct MRV32_INTLED* const INT_LEDs = (struct MRV32_INTLED*) INTERNAL_LED;
@@ -28,7 +28,7 @@ static void set_next_timer_interrupt() {
 	*mtimecmp = *mtime + 1000;  // 1000 timer ticks, corresponds to 1 MS delay with usual CLINT configuration
 }
 
-static uint8_t internal_led_state = 0;
+volatile static uint8_t internal_led_state = 0;
 void timer_irq_handler() {
 	INT_LEDs->val = internal_led_state++;
 	set_next_timer_interrupt();
@@ -68,24 +68,38 @@ void knightRider(unsigned delay) {
 
 int main() {
 
-	SWITCHES->direction = 0;
-	EXT_LEDs->direction = 1;
+	SWITCHES->direction = 0x00;
+	EXT_LEDs->direction = 0xff;
 	
 	register_timer_interrupt_handler(timer_irq_handler);
 	set_next_timer_interrupt();
 
 	printf("Init done.\n");
-
-	while(!(SWITCHES->input & 0b10000000)) {
-		printf("Running main loop\n");
-		if(SWITCHES->input & 0b00000001) {
-			printf("Running knight rider\n");
-			knightRider(2000);
+	volatile uint8_t pattern = 0x01;
+	while(1) {
+		// printf("pattern %X", pattern);
+		if(pattern == 0x80) {
+			pattern = 0x01;	
 		} else {
-			printf("Running counter\n");
-			incrementRegister(100);
+			pattern = pattern << 1;
+		} 
+		EXT_LEDs->output = pattern;
+		uint64_t now = *mtime;
+		while(now + 3000 >= *mtime) {
+			asm volatile ("nop");
 		}
 	}
+	// while(!(SWITCHES->input & 0b10000000)) {
+	// 	printf("Running main loop\n");
+	// 	if(SWITCHES->input & 0b00000001) {
+	// 		printf("Running knight rider\n");
+	// 		knightRider(2000);
+	// 	} else {
+	// 		printf("Running counter\n");
+	// 		incrementRegister(100);
+	// 	}
+	// }
+	printf("Loop done.\n");
 
 	return 0;
 }
