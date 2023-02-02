cf16d9ca7c84aaa938afa4c244f5f4fbf7ade4dd
Pascal Pieper <accounts@pascalpieper.de>
2023-02-02 17:08:22 +0100

    Improved niceness of print by 19%

--- sw/ds1302/main.cpp
+++ sw/ds1302/main.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <iostream>
+#include <iomanip>
 
 #include "arduino_gangsta_wrapper.hpp"
 #include "DS1302.h"
@@ -11,16 +12,18 @@ int main() {
 	digitalWrite(B7, HIGH);
 
 	DS1302 ds1302 (B0,B1,B2);
+	ds1302.halt(false);
 
 	printf("Init done.\n");
 
+	cout << setfill('0');
 
 	while(true) {
 		auto t = ds1302.time();
-		cout << +t.yr << "-" << +t.mon << "-" << +t.date << endl;
-		cout << +t.hr << ":" << +t.min << ":" << +t.sec << endl;
+		cout << +t.yr << "-" << setw(2) << +t.mon << "-" << setw(2) << +t.date << " ";
+		cout << setw(2) << +t.hr << ":" << setw(2) << +t.min << ":" << setw(2) << +t.sec << endl;
 
-		delayMicroseconds(100000);
+		delayMicroseconds(50000);
 	}
 
 	digitalWrite(B7, LOW);
