53d3bd7cec873f1abdcf4e58cecc8639c0855ea0
Pascal Pieper <accounts@pascalpieper.de>
2023-03-03 17:21:15 +0100

    Added sw and hw implementation

--- sw/gcd_periph/main.c
+++ sw/gcd_periph/main.c
@@ -4,7 +4,6 @@
 
 typedef uint32_t BUS_BRIDGE_TYPE;
 static volatile BUS_BRIDGE_TYPE * const GCD_HW = (BUS_BRIDGE_TYPE * const) 0x50004000;
-static const unsigned BUS_BRIDGE_ITR = 2;
 
 struct GCD_REGS {
   volatile uint32_t a;
@@ -16,30 +15,44 @@ struct GCD_REGS {
 
 static struct GCD_REGS*     const GCD_ACCEL = (struct GCD_REGS*) GCD_HW;
 
-static void set_next_timer_interrupt() {
-	*mtimecmp = *mtime + 1000;  // 1000 timer ticks, corresponds to 1 MS delay with usual CLINT configuration
+int sw_GCD(uint32_t a, uint32_t b) {
+    // thanks to https://www.programiz.com/c-programming/examples/hcf-gcd
+    while(a != b) {
+        if(a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
 }
 
-void timer_irq_handler() {
-	printf("InternalLED update\n");
-	set_next_timer_interrupt();
+int hw_GCD(uint32_t a, uint32_t b) {
+	GCD_ACCEL->a = a;
+	GCD_ACCEL->b = b;
+	GCD_ACCEL->valid = 1;
+	while(!GCD_ACCEL->ready);
+    return GCD_ACCEL->res;
 }
 
 int main() {
 	static uint32_t const a = 24;
 	static uint32_t const b = 777;
 	uint32_t result;
+    uint64_t start, end;
 
-	register_timer_interrupt_handler(timer_irq_handler);
-	set_next_timer_interrupt();
+	printf("GCD SW calculation:\n");
+	start = *mtime;
+	uint32_t res_sw = sw_GCD(a, b);
+	end = *mtime;
+	printf("done in %d us.\n", end-start); // 1000 mtime ticks correspond to 1 MS with wall-clock locked CLINT
+	printf("a: %d, b: %d, gcd(a,b)=%d\n", a, b, res_sw);
+	
+	printf("GCD SW calculation:\n");
+	start = *mtime;
+	uint32_t res_hw = hw_GCD(a, b);
+	end = *mtime;
+	printf("done in %d us.\n", end-start); // 1000 mtime ticks correspond to 1 MS with wall-clock locked CLINT
+	printf("a: %d, b: %d, gcd(a,b)=%d\n", a, b, res_hw);
 
-	printf("Init done.\n");
-	GCD_ACCEL->a = a;
-	GCD_ACCEL->b = b;
-	GCD_ACCEL->valid = 1;
-	while(!GCD_ACCEL->ready);
-	result = GCD_ACCEL->res;
-	printf("Calculation done.\n");
-	printf("a: %d, b: %d, gcd(a,b)=%d\n", a, b, result);
 	return 0;
 }
